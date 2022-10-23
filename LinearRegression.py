d8a9d2e03b96d3be23892ef9a6423053a5d6c3ee
Anden <73430931+AndreasEstmark@users.noreply.github.com>
2022-10-23 09:10:59 +0200

    Create LinearRegression.py

---
+++ LinearRegression.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+
+df["Intercept"] = 1
+#%%
+class LinearRegression:
+
+    """
+    Method to compute the parameters of linear regression and R squared. 
+    """
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def show_data(self):
+        print(self.x, self.y)
+
+    def fit(self):
+
+        x = self.x
+
+        y = self.y
+
+        beta = inv(np.transpose(x).dot(x)).dot(np.transpose(x)).dot(y)
+
+        e = y-x.dot(beta)
+
+        squaredError = (np.transpose(e).dot(e))
+
+        yDemeanedB = np.transpose(y-np.mean(y)).dot(y-np.mean(y))
+            
+        R2 = 1- (np.transpose(e).dot(e)/yDemeanedB)
+
+        return [
+            beta,
+             R2
+        ]
+
+reg = LinearRegression(df.iloc[:,1:3], df.Effect)
+
+print(reg.fit())
